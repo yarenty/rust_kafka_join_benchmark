500197ae5bda1ffac3684248ccf6817bee4b2371
Jaroslaw Nowosad <yarenty@gmail.com>
2022-02-24 15:53:24 +0000

    cconsumer

---
+++ sink/src/main.rs
@@ -0,0 +1,44 @@
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+use kafka::error::Error as KafkaError;
+
+/// This program demonstrates consuming messages through a `Consumer`.
+/// This is a convenient client that will fit most use cases.  Note
+/// that messages must be marked and commited as consumed to ensure
+/// only once delivery.
+fn main() {
+    env_logger::init();
+
+    let broker = "localhost:9092".to_owned();
+    let topic = "my-topic".to_owned();
+    let group = "my-group".to_owned();
+
+    if let Err(e) = consume_messages(group, topic, vec![broker]) {
+        println!("Failed consuming messages: {}", e);
+    }
+}
+
+fn consume_messages(group: String, topic: String, brokers: Vec<String>) -> Result<(), KafkaError> {
+    let mut con = Consumer::from_hosts(brokers)
+        .with_topic(topic)
+        .with_group(group)
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_offset_storage(GroupOffsetStorage::Kafka)
+        .create()?;
+
+    loop {
+        let mss = con.poll()?;
+        if mss.is_empty() {
+            println!("No messages available right now.");
+            return Ok(());
+        }
+
+        for ms in mss.iter() {
+            for m in ms.messages() {
+                let key = std::str::from_utf8(m.key).unwrap();
+                println!("{}:{}@{}: [{:?}] => {:?} {:?}", ms.topic(), ms.partition(), m.offset, key, m.value, std::str::from_utf8(m.value).unwrap());
+            }
+            let _ = con.consume_messageset(ms);
+        }
+        con.commit_consumed()?;
+    }
+}
